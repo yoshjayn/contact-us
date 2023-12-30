1c99a50660fb9f70c29d80ef86a6642fda137365
yoshjayn <aswiniy68@gmail.com>
2023-12-30 15:23:30 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import Nav from "./components/nav/Nav";
-import Header from "./components/header/header";
+import Nav from "./components/Nav/Nav";
+import Header from "./components/Header/Header";
 import Form from "./components/Form/Form";
 
 function App() {
